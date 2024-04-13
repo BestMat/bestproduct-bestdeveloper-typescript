ecb72c275869d8697cea7155d1e901ddff0851e5
BestMat <76582849+BestMat@users.noreply.github.com>
2024-04-13 15:58:31 +0530

    Update and rename index.js to index.ts

--- bestproduct/index.ts
+++ bestproduct/index.ts
@@ -1,6 +1,7 @@
 // ©2024 - BestDeveloper - BestMat, Inc. - All rights reserved.
 const execSync = require('child_process').execSync;
-console.log("Welcome to BestProduct by BestDeveloper!");
+var welcome_message: string = "Welcome to BestProduct by BestDeveloper!";
+console.log(welcome_message);
 
 module.exports.startDevServer = function(type, func){
     if(type == "desktop"){
